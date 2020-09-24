752ea12a920f5389a1229ac55180c0c1f97ba62c
Ksieciu <30780278+Ksieciu@users.noreply.github.com>
2020-09-24 16:31:43 +0200

    Done like/unlike working button

--- twitterclone-web/src/tweets/components.js
+++ twitterclone-web/src/tweets/components.js
@@ -23,8 +23,25 @@ export function TweetsList(props){
 
 export function ActionBtn(props) {
     const {tweet, action} = props
+    const [likes, setLikes] = useState(tweet.likes ? tweet.likes : 0)
+    const [userLike, setUserLike] = useState(tweet.userLike === true ? true : false)
     const className = props.className ? props.className : 'btn btn-primary btn-sm'
-    return action.type === 'like' ? <button className={className}>{tweet.like} Likes</button> : null
+    const actionDisplay = action.display ? action.display : 'Action'
+
+    const handleClick = (event) => {
+        event.preventDefault()
+        if (action.type === 'like'){
+            if (userLike === true){
+                setLikes(likes - 1)
+                setUserLike(false)
+            } else {
+                setLikes(likes + 1)
+                setUserLike(true)
+            }
+        }
+    }
+    const display = action.type === 'like' ? `${likes} ${actionDisplay}` : actionDisplay
+    return <button className={className} onClick={handleClick}>{display}</button>
   }
   
 export function Tweet(props){
@@ -35,8 +52,9 @@ export function Tweet(props){
       {tweet.id} - {tweet.content}
       </p>
       <div className='btn btn-group'>
-        <ActionBtn tweet={tweet} action={{type: "like"}}/> 
-        <ActionBtn tweet={tweet} action={{type: "unlike"}}/> 
+        <ActionBtn tweet={tweet} action={{type: "like", display:"Likes"}}/> 
+        <ActionBtn tweet={tweet} action={{type: "unlike", display:"Unlike"}}/> 
+        <ActionBtn tweet={tweet} action={{type: "unlike", display:"Retweet"}}/> 
       </div>
     </div>
   }
